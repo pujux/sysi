b3fe2b320506172060bec94a8d2c5517f427e16d
Julian Pufler <puf17640@spengergasse.at>
2020-07-31 11:04:32 +0200

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const info = require('systeminformation'),
 const [,, configPath = './config.json'] = process.argv,
 	{ settings, parts } = require(configPath);
 
-settings.seperator = settings.seperator.repeat(settings.seperatorLength)
+settings.seperator = settings.seperator.repeat(settings.seperatorLength);
 
 const formatBytes = (bytes = 0, withGb) => {
 	const sizes = ['B', 'KB', 'MB', 'GB'];
@@ -26,153 +26,157 @@ const formatDuration = (duration) => {
 };
 
 (async () => {
-
 	const lines = (await Promise.all(parts.map(async ({ name, enabled, ...args }) => {
 		let data;
-		switch (name) {
-			case 'seperator':
-				if (!enabled) break;
-				return ['seperator', ''];
+		try {
+			switch (name) {
+				case 'seperator':
+					if (!enabled) break;
+					return ['seperator', ''];
 
-			case 'os':
-				if (!enabled) break;
-				data = await info.osInfo();
-				if (!data || !data.distro) break;
-				return [
-					chalk.hex(settings.primaryColor)('OS'),
-					`${data.distro}${args.version && data.release ? `, ${data.release}` : ''}${args.arch && data.arch ? ` (${data.arch})` : ''}`
-				];
+				case 'os':
+					if (!enabled) break;
+					data = await info.osInfo();
+					if (!data || !data.distro) break;
+					return [
+						chalk.hex(settings.primaryColor)('OS'),
+						`${data.distro}${args.version && data.release ? `, ${data.release}` : ''}${args.arch && data.arch ? ` (${data.arch})` : ''}`
+					];
 
-			case 'cpu':
-				if (!enabled) break;
-				data = { ...await info.cpu(), temp: (await info.cpuTemperature()).main };
-				if (!data || !data.manufacturer || !data.brand) break;
-				return [
-					chalk.hex(settings.primaryColor)('CPU'),
-					`${data.manufacturer} ${data.brand}${args.cores && data.physicalCores > 0 ? ` (${data.physicalCores})` : ''}${args.speed && data.speed > 0 ? ` @ ${data.speed}GHz` : ''}${args.temp && data.temp > 0 ? ` (${data.temp} °C)` : ''}`
-				];
+				case 'cpu':
+					if (!enabled) break;
+					data = { ...await info.cpu(), temp: (await info.cpuTemperature()).main };
+					if (!data || !data.manufacturer || !data.brand) break;
+					return [
+						chalk.hex(settings.primaryColor)('CPU'),
+						`${data.manufacturer} ${data.brand}${args.cores && data.physicalCores > 0 ? ` (${data.physicalCores})` : ''}`
+						+ `${args.speed && data.speed > 0 ? ` @ ${data.speed}GHz` : ''}${args.temp && data.temp > 0 ? ` (${data.temp} °C)` : ''}`
+					];
 
-			case 'uptime':
-				if (!enabled) break;
-				data = info.time();
-				if (!data || !data.uptime || data.uptime < 0)	break;
-				return [
-					chalk.hex(settings.primaryColor)('Uptime'),
-					`${formatDuration(moment.duration(data.uptime, 'seconds'))}`
-				];
+				case 'uptime':
+					if (!enabled) break;
+					data = info.time();
+					if (!data || !data.uptime || data.uptime < 0)	break;
+					return [
+						chalk.hex(settings.primaryColor)('Uptime'),
+						`${formatDuration(moment.duration(data.uptime, 'seconds'))}`
+					];
 
-			case 'gpu':
-				if (!enabled) break;
-				data = (await info.graphics()).controllers;
-				if (!data || data.length === 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('GPU'),
-					`${data.filter(con => con.model).map(con => `${con.model}${args.vram && con.vram > 0 ? ` (${formatBytes(con.vram * 1024 ** 2, true)})` : ''}`).join(', ')}`
-				];
+				case 'gpu':
+					if (!enabled) break;
+					data = (await info.graphics()).controllers;
+					if (!data || data.length === 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('GPU'),
+						`${data.filter(con => con.model).map(con => `${con.model}${args.vram && con.vram > 0 ? ` (${formatBytes(con.vram * 1024 ** 2, true)})` : ''}`).join(', ')}`
+					];
 
-			case 'memory':
-				if (!enabled) break;
-				data = await info.mem();
-				if (!data || !data.active || data.active < 0 || !data.total || data.total < 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Memory'),
-					`${formatBytes(data.active)} / ${formatBytes(data.total)}${args.percent ? ` (${(data.active / data.total).toFixed(2) * 100} %)` : ''}`
-				];
+				case 'memory':
+					if (!enabled) break;
+					data = await info.mem();
+					if (!data || !data.active || data.active < 0 || !data.total || data.total < 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Memory'),
+						`${formatBytes(data.active)} / ${formatBytes(data.total)}${args.percent ? ` (${(data.active / data.total).toFixed(2) * 100} %)` : ''}`
+					];
 
-			case 'display':
-				if (!enabled) break;
-				data = (await info.graphics()).displays;
-				if (!data || data.length === 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Display'),
-					`${data.filter(dis => !args.mainOnly || dis.main).map(dis => `${dis.resolutionx}x${dis.resolutiony}`).join(', ')}`
-				];
+				case 'display':
+					if (!enabled) break;
+					data = (await info.graphics()).displays;
+					if (!data || data.length === 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Display'),
+						`${data.filter(dis => !args.mainOnly || dis.main).map(dis => `${dis.resolutionx}x${dis.resolutiony}`).join(', ')}`
+					];
 
-			case 'proc':
-				if (!enabled) break;
-				data = await info.processes();
-				if (!data || !data.all || data.all < 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Processes'),
-					data.all
-				];
+				case 'proc':
+					if (!enabled) break;
+					data = await info.processes();
+					if (!data || !data.all || data.all < 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Processes'),
+						data.all
+					];
 
-			case 'battery':
-				if (!enabled) break;
-				data = await info.battery();
-				if (!data || !data.percent || data.percent < 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Battery'),
-					`${data.percent} %${args.timeRemaining ? ` (${data.ischarging ? 'charging' : formatDuration(moment.duration(data.timeremaining, 'minutes'))})` : ''}`
-				];
+				case 'battery':
+					if (!enabled) break;
+					data = await info.battery();
+					if (!data || !data.percent || data.percent < 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Battery'),
+						`${data.percent} %${args.timeRemaining ? ` (${data.ischarging ? 'charging' : formatDuration(moment.duration(data.timeremaining, 'minutes'))})` : ''}`
+					];
 
-			case 'ping':
-				if (!enabled) break;
-				data = await info.inetLatency();
-				if (!data || data < 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Ping'),
-					`${data.toFixed(0)} ms`
-				];
+				case 'ping':
+					if (!enabled) break;
+					data = await info.inetLatency();
+					if (!data || data < 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Ping'),
+						`${data.toFixed(0)} ms`
+					];
 
-			case 'publicIp':
-				if (!enabled) break;
-				data = await publicIp.v4();
-				if (!data) break;
-				return [
-					chalk.hex(settings.primaryColor)('Public IP'),
-					data
-				];
+				case 'publicIp':
+					if (!enabled) break;
+					data = await publicIp.v4();
+					if (!data) break;
+					return [
+						chalk.hex(settings.primaryColor)('Public IP'),
+						data
+					];
 
-			case 'net':
-				if (!enabled) break;
-				data = await info.networkInterfaces();
-				if (!data || data.length === 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Net'),
-					data.filter(net => net.ip4 && net.ifaceName && (args.noLocal && net.ifaceName !== 'lo0' || !args.noLocal)).map(net => `${net.ip4} (${net.ifaceName})`).join(', ')
-				];
+				case 'net':
+					if (!enabled) break;
+					data = await info.networkInterfaces();
+					if (!data || data.length === 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Net'),
+						data.filter(net => net.ip4 && net.ifaceName && (args.noLocal && net.ifaceName !== 'lo0' || !args.noLocal)).map(net => `${net.ip4} (${net.ifaceName})`).join(', ')
+					];
 
-			case 'shell':
-				if (!enabled) break;
-				data = await info.shell();
-				if (!data) break;
-				return [
-					chalk.hex(settings.primaryColor)('Shell'),
-					data
-				];
+				case 'shell':
+					if (!enabled) break;
+					data = await info.shell();
+					if (!data) break;
+					return [
+						chalk.hex(settings.primaryColor)('Shell'),
+						data
+					];
 
-			case 'users':
-				if (!enabled) break;
-				data = await info.users();
-				if (!data || data.length === 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('Online'),
-					[...new Set(data.map(user => user.user).filter(user => user))].join(', ')
-				];
+				case 'users':
+					if (!enabled) break;
+					data = await info.users();
+					if (!data || data.length === 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('Online'),
+						[...new Set(data.map(user => user.user).filter(user => user))].join(', ')
+					];
 
-			case 'cpuLoad':
-				if (!enabled) break;
-				data = await info.currentLoad();
-				if (!data || !data.currentload || data.currentload < 0) break;
-				return [
-					chalk.hex(settings.primaryColor)('CPU Load'),
-					`${data.currentload.toFixed(2)} %`
-				];
+				case 'cpuLoad':
+					if (!enabled) break;
+					data = await info.currentLoad();
+					if (!data || !data.currentload || data.currentload < 0) break;
+					return [
+						chalk.hex(settings.primaryColor)('CPU Load'),
+						`${data.currentload.toFixed(2)} %`
+					];
+			}
+			return undefined;
+		} catch {
+			return undefined;
 		}
-		return undefined;
 	})))
-	.filter(line => line)
-	.map(([a, b]) => [
-		a !== 'seperator' ? a + chalk.hex(settings.suffixColor)(settings.suffix) : a, 
-		chalk.hex(settings.secondaryColor)(b)
-	]);
+		.filter(line => line)
+		.map(([a, b]) => [
+			a !== 'seperator' ? a + chalk.hex(settings.suffixColor)(settings.suffix) : a,
+			chalk.hex(settings.secondaryColor)(b)
+		]);
 	let text = '';
-	if(settings.title) {
+	if (settings.title) {
 		const { osInfo: { hostname } } = await info.get({ osInfo: 'hostname' });
 		text = `${chalk.hex(settings.titleColor)(hostname)}\nseperator\n`;
 	}
-	text += table(lines)
+	text += table(lines);
 	text = text.replace(/seperator/g, chalk.hex(settings.seperatorColor)(settings.seperator));
-	console.log(text);
+	console.log(text, '\n');
 })();
